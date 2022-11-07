e3978328e3b5ea793f55c28aa220efa25e41fdb7
sabbunsopum <sabbunsopum@gmail.com>
2022-11-07 14:11:46 +0900

    221107

---
+++ src/components/include/MovieList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MovieList = () => {
+  return (
+    <div className="movie__list">
+      <div className="container">
+        <h2>Best</h2>
+        <div className="movie__hot"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
--- src/components/pages/Movie.jsx
+++ src/components/pages/Movie.jsx
@@ -1,26 +1,46 @@
-import React from "react";
-import { useState, useEffect } from "react";
+// import React from "react";
 import Header from "../layout/Header";
-import Title from "../layout/Title";
-import MovieCont from "../include/MovieCont";
 import Contents from "../layout/Contents";
 import Footer from "../layout/Footer";
+import Title from "../layout/Title";
+import MovieCont from "../include/MovieCont";
+import Contact from "../layout/Contact";
+import MovieSearch from "../include/MovieSearch";
+import MovieList from "../include/MovieList";
+import { useState, useEffect } from "react";
 const Movie = () => {
   const [movies, setMovies] = useState([]);
-  useEffect(() => {
+
+  const search = (query) => {
     fetch(
-      "https://api.themoviedb.org/3/search/movie?api_key=25947345ed29186e578a0e972443bc29&query=marble&page=1"
+      `https://api.themoviedb.org/3/search/movie?api_key=25947345ed29186e578a0e972443bc29&query=${query}`
     )
       .then((response) => response.json())
-      .then((result) => setMovies(result.data.results))
-      .catch((error) => console.log("error", error));
+      .then((result) => setMovies(result.results))
+      .catch((error) => console.log(error));
+  };
+  //자바스크립트
+
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/search/movie?api_key=25947345ed29186e578a0e972443bc29&query=marvel"
+    )
+      .then((response) => response.json())
+      .then((result) => setMovies(result.results))
+      .catch((error) => console.log(error));
   }, []);
+  //리액트
+
   return (
     <>
       <Header />
-      <Title title={["Movie", "Movie api"]} />
-      <MovieCont movies={movies} />
-      <Contents />
+      <Contents>
+        <Title title={["Movie", "Reference api"]} />
+        <MovieList />
+        <MovieSearch onSearch={search} />
+        <MovieCont movies={movies} />
+        <Contact />
+      </Contents>
       <Footer />
     </>
   );
